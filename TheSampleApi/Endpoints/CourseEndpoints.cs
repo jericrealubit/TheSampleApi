624f880921c93eb23378de6be784adb63e1d8435
jeric realubit <jericrealubit@gmail.com>
2025-11-23 20:44:11 +0800

    search and load by id

--- TheSampleApi/Endpoints/CourseEndpoints.cs
+++ TheSampleApi/Endpoints/CourseEndpoints.cs
@@ -6,9 +6,44 @@ public static class CourseEndpoints
 {
     public static void AddCourseEndpoints(this WebApplication app)
     {
-        app.MapGet("/courses", (CourseData data) =>
+        app.MapGet("/courses", LoadAllCourses);
+        app.MapGet("/courses/{id}", LoadCourseById);
+    }
+
+    private static IResult LoadAllCourses(
+        CourseData data,
+        string? courseType,
+        string? search
+    )
+    {
+        var output = data.Courses;
+
+        if (string.IsNullOrWhiteSpace(courseType) == false) 
         {
-            return data.Courses;
-        });
+            output.RemoveAll(x => string.Compare(
+                x.CourseType, 
+                courseType, 
+                StringComparison.OrdinalIgnoreCase) != 0);
+        }
+
+        if (string.IsNullOrWhiteSpace(search) == false)
+        {
+            output.RemoveAll(x => 
+                !x.CourseName.Contains(search, StringComparison.OrdinalIgnoreCase) &&
+                !x.ShortDescription.Contains(search, StringComparison.OrdinalIgnoreCase));
+        }
+
+        return Results.Ok(output);
+    }
+
+    private static IResult LoadCourseById(CourseData data, int id)
+    {
+        var output = data.Courses.SingleOrDefault(x => x.Id == id);
+        if (output is null)
+        {
+            return Results.NotFound();
+        }
+
+        return Results.Ok(output);
     }
 }
